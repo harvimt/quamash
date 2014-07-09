5477ecf5569f7332d294571a0934a54cdeb57c85
Arve Knudsen <arve.knudsen@gmail.com>
2014-07-09 22:12:03 +0200

    Fix setup

--- setup.py
+++ setup.py
@@ -1,16 +1,18 @@
 from distutils.core import setup
 import quamash
 import re
-author = re.match(r'(?P<name>[^<]* <(?P<email>.*)>', quamash.__author__)
 
+
+groups = re.findall(r'(.+?) <(.+?)>(?:,\s*)?', quamash.__author__)
+authors = [x[0].strip() for x in groups]
+emails = [x[1].strip() for x in groups]
 setup(
     name='Quamash',
     version=quamash.__version__,
-    author=author.group('name'),
-    author_email=author.group('email'),
+    author=', '.join(authors),
+    author_email=', '.join(emails),
     packages=['quamash', ],
     license=quamash.__license__,
-    depends=['tulip', 'PyQt', ],
     description=quamash.__doc__,
     long_description=open('README').read(),
 )
