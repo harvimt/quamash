24ed64988b8446d98bed652c980478689e246651
Arve Knudsen <arve.knudsen@gmail.com>
2014-07-21 21:33:35 +0200

    Ignore Windows logic on non-Windows and vice versa

--- conftest.py
+++ conftest.py
@@ -5,4 +5,7 @@ sys.path.insert(0, os.path.dirname(__file__))
 logging.basicConfig(
 	level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(name)s - %(message)s')
 
-
+if os.name == 'nt':
+    collect_ignore = ['quamash/_unix.py']
+else:
+    collect_ignore = ['quamash/_windows.py']
