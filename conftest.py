c7eedc602284052eae7fd2623c4a580d10d48844
Arve Knudsen <arve.knudsen@gmail.com>
2014-11-24 11:45:14 +0100

    Fix whitespace

--- conftest.py
+++ conftest.py
@@ -6,6 +6,6 @@ logging.basicConfig(
 	level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(name)s - %(message)s')
 
 if os.name == 'nt':
-    collect_ignore = ['quamash/_unix.py']
+	collect_ignore = ['quamash/_unix.py']
 else:
-    collect_ignore = ['quamash/_windows.py']
+	collect_ignore = ['quamash/_windows.py']
