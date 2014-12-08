cc2c1c67e15c2058f119a4d6ab743d394b2c9406
Mark Harviston <mark.harviston@gmail.com>
2014-12-08 13:34:38 -0800

    Fix indentation in setup.py

--- setup.py
+++ setup.py
@@ -6,15 +6,15 @@ groups = re.findall(r'(.+?) <(.+?)>(?:,\s*)?', quamash.__author__)
 authors = [x[0].strip() for x in groups]
 emails = [x[1].strip() for x in groups]
 setup(
-    name='Quamash',
-    version=quamash.__version__,
+	name='Quamash',
+	version=quamash.__version__,
 	url='https://github.com/harvimt/quamash',
-    author=', '.join(authors),
-    author_email=', '.join(emails),
-    packages=['quamash', ],
-    license=quamash.__license__,
-    description=quamash.__doc__,
-    long_description=open('README').read(),
+	author=', '.join(authors),
+	author_email=', '.join(emails),
+	packages=['quamash', ],
+	license=quamash.__license__,
+	description=quamash.__doc__,
+	long_description=open('README').read(),
 	keywords=['Qt', 'asyncio'],
 	classifiers=[
 		'License :: OSI Approved :: BSD License',
