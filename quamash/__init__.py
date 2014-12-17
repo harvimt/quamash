harvimt/quamash

commit c41268758b8330be85cf1d0615502a8dca669887

Disable QSocketNotifier while calling read/write callbacks

quamash/__init__.py
@@ -337,7 +337,9 @@ class QEventLoop(_baseclass):
 		notifier = QtCore.QSocketNotifier(fd, QtCore.QSocketNotifier.Read)
 		notifier.setEnabled(True)
 		self._logger.debug('Adding reader callback for file descriptor {}'.format(fd))
-		notifier.activated.connect(lambda: callback(*args))
+		notifier.activated.connect(
+			lambda: self.__on_notifier_ready(notifier, callback, args)
+		)
 		self._read_notifiers[fd] = notifier
 
 	def remove_reader(self, fd):
@@ -351,7 +353,9 @@ class QEventLoop(_baseclass):
 		notifier = QtCore.QSocketNotifier(fd, QtCore.QSocketNotifier.Write)
 		notifier.setEnabled(True)
 		self._logger.debug('Adding writer callback for file descriptor {}'.format(fd))
-		notifier.activated.connect(lambda: callback(*args))
+		notifier.activated.connect(
+			lambda: self.__on_notifier_ready(notifier, callback, args)
+		)
 		self._write_notifiers[fd] = notifier
 
 	def remove_writer(self, fd):
@@ -359,6 +363,17 @@ class QEventLoop(_baseclass):
 		self._logger.debug('Removing writer callback for file descriptor {}'.format(fd))
 		notifier = self._write_notifiers.pop(fd)
 		notifier.setEnabled(False)
+
+	@staticmethod
+	def __on_notifier_ready(notifier, callback, args):
+		# It can be necessary to disable QSocketNotifier when e.g. checking
+		# ZeroMQ sockets for events
+		enabled = notifier.isEnabled()
+		notifier.setEnabled(False)
+		try:
+			callback(*args)
+		finally:
+			notifier.setEnabled(enabled)
 
 	# Methods for interacting with threads.
 

commit 8cc7f48b0d636eafa7963bcd0e598415301e4eb6

Fix removal of r/w-notifiers if another notifier is still bound
I think this is a regression causd by c412687. That commit adds, a
wrapper for the callback provided by the user. The workaround supplied
by 43256c0 is not sufficient, and this commit aims to fix this.

A common situation to trigger this is the asyncio-supplied SSL
implementation.

quamash/__init__.py
@@ -363,7 +363,8 @@ class QEventLoop(_baseclass):
 		notifier.setEnabled(True)
 		self._logger.debug('Adding reader callback for file descriptor {}'.format(fd))
 		notifier.activated.connect(
-			lambda: self.__on_notifier_ready(notifier, fd, callback, args)
+			lambda: self.__on_notifier_ready(self._read_notifiers,
+							 notifier, fd, callback, args)
 		)
 		self._read_notifiers[fd] = notifier
 
@@ -385,7 +386,8 @@ class QEventLoop(_baseclass):
 		notifier.setEnabled(True)
 		self._logger.debug('Adding writer callback for file descriptor {}'.format(fd))
 		notifier.activated.connect(
-			lambda: self.__on_notifier_ready(notifier, fd, callback, args)
+			lambda: self.__on_notifier_ready(self._write_notifiers,
+							 notifier, fd, callback, args)
 		)
 		self._write_notifiers[fd] = notifier
 
@@ -401,8 +403,8 @@ class QEventLoop(_baseclass):
 		else:
 			notifier.setEnabled(False)
 
-	def __on_notifier_ready(self, notifier, fd, callback, args):
-		if fd not in self._read_notifiers and fd not in self._write_notifiers:
+	def __on_notifier_ready(self, notifiers, notifier, fd, callback, args):
+		if fd not in notifiers:
 			self._logger.warning(
 				'Socket notifier for fd {} is ready, even though it should be disabled, not calling {} and disabling'
 				.format(fd, callback)
@@ -418,7 +420,8 @@ class QEventLoop(_baseclass):
 		try:
 			callback(*args)
 		finally:
-			notifier.setEnabled(True)
+			if fd in notifiers:
+				notifier.setEnabled(True)
 
 	# Methods for interacting with threads.
 