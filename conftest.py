5c93eb66a97b6deb3709fd3fca6861b60042ddfb
Arve Knudsen <arve.knudsen@gmail.com>
2015-01-17 22:26:47 +0100

    Instantiate QApplication once for all (doc)tests

--- conftest.py
+++ conftest.py
@@ -1,6 +1,7 @@
 import sys
 import os.path
 import logging
+from pytest import fixture
 sys.path.insert(0, os.path.dirname(__file__))
 logging.basicConfig(
 	level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(name)s - %(message)s')
@@ -9,3 +10,14 @@ if os.name == 'nt':
 	collect_ignore = ['quamash/_unix.py']
 else:
 	collect_ignore = ['quamash/_windows.py']
+
+_app = None
+
+
+@fixture
+def app():
+	from quamash import QApplication
+	global _app
+	if _app is None:
+		_app = QApplication([])
+	return _app
