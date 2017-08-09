eff48f2600899672041411012b51becc90e653ea
Korijn van Golen <korijn@gmail.com>
2017-08-09 08:49:44 +0200

    strip whitespace

--- quamash/_unix.py
+++ quamash/_unix.py
@@ -217,5 +217,5 @@ class _SelectorEventLoop(asyncio.SelectorEventLoop):
 				self._logger.debug('Invoking writer callback: {}'.format(writer))
 				writer._run()
 
-				
+
 baseclass = _SelectorEventLoop
